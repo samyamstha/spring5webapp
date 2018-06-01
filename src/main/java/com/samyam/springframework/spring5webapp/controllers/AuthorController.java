1be5030a949c483affb794462f198ef471dd67b5
samyamstha <samyamsth@gmail.com>
2018-06-01 13:45:08 -0400

    Authors Controller to get list of the authors.

---
+++ src/main/java/com/samyam/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package com.samyam.springframework.spring5webapp.controllers;
+
+import com.samyam.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping(value = "/authors")
+    public String getAuthor(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
